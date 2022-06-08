0807a1bd5516b664a42a6b95d919efa6bbdaf38b
Misael Primaz <misael-primaz@procergs.rs.gov.br>
2022-06-08 18:18:16 -0300

    Versão 0.1 onu_showall_slot.textfsm

--- textfsm_lab/main.py
+++ textfsm_lab/main.py
@@ -6,12 +6,27 @@ files_path = f'{local_path}/outputs'
 templates_path = f'{local_path}/templates'
 files = os.listdir(files_path)
 
-for file in files:
-    with open(f'{files_path}/{file}') as f:
-        output = f.read()
-    print(output)
-    with open(f"{templates_path}/{file.replace('.txt', '.textfsm')}") as t:
-        template = textfsm.TextFSM(t)
-    fsm_result = template.ParseText(output)
-    print(f'Result: {fsm_result}')
-    input('Pressione algo para ir para o próximo arquivo...')
+
+file = "onu_showall_slot.txt"
+
+with open(f'{files_path}/{file}') as f:
+    output = f.read()
+print(output)
+with open(f"{templates_path}/{file.replace('.txt', '.textfsm')}") as t:
+    template = textfsm.TextFSM(t)
+fsm_result = template.ParseText(output)
+print(f'Headers: {template.header}')
+
+showall = []
+for row in fsm_result:
+    d = {}
+    row[3] = row[3].replace(' ', '')
+    row[4] = row[4].replace('ZNTS ', '')
+    row[4] = row[4].replace(' ', '')
+    for i, value in enumerate(row):
+        d.update({template.header[i]: row[i]})
+    showall.append(d)
+
+print([(registro['name'], registro['serial']) for registro in showall if registro.get('serial')])
+
+#   ^\s+${id}\s+${name}\s+${enabled}\s${model}\s+${serial}\s+${omci} -> Record
